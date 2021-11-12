3cbb92d3762749f033c477fc8394d49afabb643d
AmnaSZafar <amna.s.zafar@gmail.com>
2021-11-12 10:44:33 +0500

    fc

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -11,8 +11,13 @@ function Home() {
                 <div className="row">
                     <div className="col-lg-6 d-lg-flex flex-lg-column justify-content-center align-items-stretch pt-5 pt-lg-0 order-2 order-lg-1" data-aos="fade-up">
                     <div>
-                        <h1>State-of-the-art Digital School</h1>
-                        <h2>Didactics is an online, digital school, using all of the latest technology to allow everybody access to education.</h2>
+                        <h1>DIDACTICS<br /> International Digital School</h1>
+                        <h2>Didactics is the first digital School in Pakistan, offering classes from Playgroup to 
+                        Grade 12. We follow the International standards for our students. We focus on providing quality
+                         education and paying attention to each individual student. Our programmes are designed to stimulate
+                          each child’s development and learning and provide a platform for their future schooling. 
+                          Our digital portal uses state of the art teachnologies. With our digital portal, a student can study remotely anywhere in Pakistan.
+                          </h2>
                         
                     </div>
                     </div>
@@ -29,7 +34,17 @@ function Home() {
           <div className="section-title">
           
             <h2>App Features</h2>
-            <p>Didactics uses state-of-the-art technology to allow everybody access to education. The team at Didactics are passionate about enabling everybody to learn in the best way possible and are constantly updating the features of the platform to ensure that the students are receiving high quality education. See some of our main features below.</p>
+            <p>
+            Didactics is well ahead of its competitors, in terms of education, entrepreneurship, 
+            economic competitiveness and quality of life. At Didactics International School, we take 
+            advantage of effective teaching practices developed around the world and teach our students 
+            with the same quality, discipline, and ethics. Didactics International School is a place where 
+            students can achieve their full potential. We provide a calm and safe study environment that 
+            favors learning, with a focus on knowledge, skills and academic progress. Our teachers concentrate 
+            on challenging and interacting with students, preparing them for a life in an increasingly globalized world.
+            Didactics is passionate about enabling everybody to learn in the best way possible and is constantly 
+            updating the features of the platform to ensure that the students are 
+            receiving high quality education. See some of our main features below.</p>
           </div>
   
         </div>
@@ -44,6 +59,9 @@ function Home() {
               <h3>Tablet Based.</h3>
               <p className="fst-italic">
               Study using a tablet and write in any language. Practise and improve your handwriting whilst studying from any location.
+              With Didactics, you would not need any notebooks, stationery or hard-copy. Our digital portal at Didactics has integrated everything that 
+              you might need. Online books are provided instead of hard-copy books. Online notepads and drawing boards are provided thus eliminating the need for stationery
+              and notebooks.
               </p>
             </div>
           </div>
@@ -54,7 +72,11 @@ function Home() {
             <div className="col-md-8 pt-5 order-2 order-md-1" data-aos="fade-up">
               <h3>Built-in Video Classes.</h3>
               <p className="fst-italic">
-              Study from anywhere at a time suitable for you with your teacher, just as if you are in the classroom with them.
+              Study from anywhere at any time that is suitable for you with your teacher. With our digital portal at Didactics, you
+              would feel as if you in a physical class with the teacher. Our class timings are completely flexible for you and the teacher.
+              Our staff is patient and extremely understanding. Apart from built-in video classes, you can also have a video conference with your teacher any time 
+              if you are facing any issue. 
+
               </p>
             </div>
           </div>
@@ -64,7 +86,10 @@ function Home() {
             </div>
             <div className="col-md-8 pt-5" data-aos="fade-up">
               <h3>Video and Audio Messaging.</h3>
-              <p>Whenever you need anything, you can communicate with a teacher 24 hours a day, 7 days a week.</p>
+              <p>Whenever you need anything, you can communicate with a teacher 24 hours a day, 7 days a week. Didactics digital portal system is unlike any 
+              other. No other school offers 24/7 communication like Didactics. Instead of talking with your friend about an answer, ask the teacher directly.
+              Our teachers are available 24/7 to help out the students as much as they can.
+              </p>
               
             </div>
           </div>
@@ -75,7 +100,10 @@ function Home() {
             <div className="col-md-8 pt-5 order-2 order-md-1" data-aos="fade-up">
               <h3>Digital Books.</h3>
               <p className="fst-italic">
-              Interactive learning material and content in order to stimulate students while teaching new concepts.
+              All Interactive learning material and content is placed under one roof.
+              Didactics offers Online Books to all its students. The benefits of having all e-books in one place helps out the students
+              to study effectively. This is not only environmently friendly but also extremely beneficial for all students. Students can 
+              save bookmarks in all e-books. Students can share the book page with the teacher for if there is something that they don't understand.
               </p>
             </div>
           </div>
@@ -90,7 +118,10 @@ function Home() {
             <div className="col-md-8 pt-4" data-aos="fade-up">
               <h3>Digital Notebook.</h3>
               <p className="fst-italic">
-              Keep all your notes in one online place and never lose that crucial piece of paper before the exam.
+              Didactics digital portal eliminates the need for all types of stationery. <br />
+              Didactics provides digital notebooks.
+              These online notebooks are all stored in our digital portal. Due to this the student may never have to go through the 
+              crucial period of loosing their notes before their exams.
               </p>
             </div>
           </div>
@@ -101,7 +132,13 @@ function Home() {
             <div className="col-md-8 pt-5 order-2 order-md-1" data-aos="fade-up">
               <h3>Group Self-Study Classes.</h3>
               <p className="fst-italic">
-              Study with other students from all around the world at a convenient time for you to help support each other’s learning.
+              Study with other students from all around the world.
+              You can have video calls and meetings with your friends to help support each other’s learning.
+              <br />
+              You and your friends can use our in-built group self-study classes to have discussions regarding class homework or just make new friends.
+              <br/>
+              With our group classes, you can get familiarized with other students, discuss problems and find solutions together.
+              This encourages team-work amongst the students.
               </p>
             
             </div>
@@ -112,7 +149,12 @@ function Home() {
             </div>
             <div className="col-md-8 pt-5" data-aos="fade-up">
               <h3>Online Testing.</h3>
-              <p>Testing is very important in all learning. Online, time-bounded testing allows you to take the test from anywhere in the world.</p>
+              <p>Testing is very important at Didactics. We test our students completely online. We have time-bounded tests that allows us to include students from all areas
+              students all over the world to participate in the same test.
+              Our team at Didactics has implemented various measures to ensure that all tests are given ethically. We use artificial intelligence 
+              to compare tests of all students, thus minimizing the chances for cheating. 
+
+              </p>
              
             </div>
           </div>
@@ -123,7 +165,9 @@ function Home() {
             <div className="col-md-8 pt-5 order-2 order-md-1" data-aos="fade-up">
               <h3>Facial Recognition.</h3>
               <p className="fst-italic">
-              State-of-the-art facial recognition system to check attendance in class, time spent studying and to validate testing.
+              We use state of the art facial recognition system to check attendance in class. 
+              We also check if the student is attentive in the class or not. If the student is inactive, our software notices that and informs the teacher.
+              This monitoring system allwos teachers to save up on time and invest more time on teaching and validate testing.
              </p>
             </div>
           </div>
@@ -139,6 +183,8 @@ function Home() {
               <h3>Performance Reports.</h3>
               <p className="fst-italic">
               Regular performance reports will be sent based on artificial intelligence and various analytics, including how active students are in lessons.
+              Apart from that, Student's ethical bahavior in the class and with other students in group study classes will also be noted. 
+              Students performance reports will be sent to the administration and to the parents.
               </p>
               
             </div>
